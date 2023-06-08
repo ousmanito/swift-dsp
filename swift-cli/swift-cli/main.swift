99b379b40b64d5ed4f695bd7d2b3f86f91fad521
Utilisateur invité <Guest@iMac-de-admin-106.local>
2023-06-08 10:26:55 +0200

    [feat] :sparkles: create eval function for arithmetical operations

--- swift-cli/swift-cli/main.swift
+++ swift-cli/swift-cli/main.swift
@@ -2,17 +2,80 @@
 //  main.swift
 //  swift-cli
 //
-//  Created by Utilisateur invité on 06/06/2023.
-//
+//  Created by Ousmane Bathily le 06/06/2023.
 
 import Foundation
 
-var variable = "Florent"
-let const = "Hello"
-
-if !variable.isEmpty {
-    print("Variable is empty")
+class Parser {
+    let s: String
+    var index: String.Index
+    init(_ s: String) {
+        self.s = s
+        self.index = s.startIndex
+    }
+    
+    func head() -> Character? {
+        return index < s.endIndex ? s[index] : nil
+    }
+    
+    func pop(_ char: Character) {
+        assert(head() == char)
+        index = s.index(after: index)
+    }
+    
+    func parse() -> Int {
+        let result = sum()
+        assert(head() == nil)
+        return result
+    }
+    
+    func sum() -> Int {
+        var result = product()
+        while let char = head(), char == "+" || char == "-" {
+            if char == "+" {
+                pop("+")
+                result += product()
+            } else {
+                pop("-")
+                result -= product()
+            }
+        }
+        return result
+    }
+    
+    func product() -> Int {
+        var result = self.getFactor()
+        while let char = head(), char == "*" {
+            pop("*")
+            result *= getFactor()
+        }
+        return result
+    }
+    
+    func getFactor() -> Int {
+        if let char = head(), char.isNumber {
+            return number()
+        } else if head() == "(" {
+            pop("(")
+            let result = sum()
+            pop(")")
+            return result
+        } else {
+            fatalError("Erreur de syntaxe: Vérifier l'expression")
+        }
+    }
+    
+    func number() -> Int {
+        var numStr = ""
+        while let char = head(), char.isNumber {
+            numStr.append(char)
+            index = s.index(after: index)
+        }
+        return Int(numStr)!
+    }
 }
 
-print("Hello, World!")
-
+func eval(_ s: String) -> Int {
+    let parser = Parser(s)
+    return parser.parse()
+}
